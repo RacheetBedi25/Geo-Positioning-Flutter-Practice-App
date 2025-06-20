bc38c96937f85229c417b7b83211198665638e84
Rohan-Prabhala <166223657+Rohan-Prabhala@users.noreply.github.com>
2025-06-19 22:52:15 -0500

    info button
    Info button has been created
    added an example gif - should be replaced later with a visual example of the instructions
    
    To-Do
    - delete current text and gifs in info
    - add multiple visual example gifs later
    - add instructions for the app
    - clean up code and use proper formatting + structuring
    - add menu button functionality (make them act go to their pages)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 // https://pub.dev/packages/geolocator
@@ -17,6 +18,8 @@ void main() {
 }
 
 class Home extends StatelessWidget {
+  const Home({super.key});
+
   final double topGap = 200.0;
   final double buttonGap = 30.0;
 
@@ -54,7 +57,7 @@ class Home extends StatelessWidget {
         ),
         actions: [
           IconButton(
-            onPressed: infoButtonPressed,
+            onPressed: () => infoButtonPressed(context),
             alignment: Alignment.center,
             iconSize: 40.0,
             icon: Icon(Icons.info),
@@ -80,13 +83,13 @@ class Home extends StatelessWidget {
           createButtonRow(
             MainAxisAlignment.center,
             EdgeInsets.only(top: buttonGap),
-            geoFenceButtonPressed,
+            settingsButtonPressed,
             'Settings',
           ),
           createButtonRow(
             MainAxisAlignment.center,
             EdgeInsets.only(top: buttonGap),
-            geoFenceButtonPressed,
+            moreButtonPressed,
             'More',
           ),
         ],
@@ -111,7 +114,86 @@ class Home extends StatelessWidget {
     );
   }
 
-  void infoButtonPressed() {}
+  void infoButtonPressed(BuildContext context) {
+    showGeneralDialog(
+      barrierColor: Colors.black.withAlpha(125),
+      transitionBuilder: (context, a1, a2, widget) {
+        final curvedValue = Curves.easeInOutBack.transform(a1.value) - 1.0;
+        return Transform(
+          transform: Matrix4.translationValues(0.0, curvedValue * 400, 0.0),
+          child: Opacity(opacity: a1.value, child: buildInfoPopup()),
+        );
+      },
+      transitionDuration: Duration(milliseconds: 400),
+      barrierDismissible: true,
+      barrierLabel: '',
+      context: context,
+      pageBuilder: (context, animation, secondaryAnimation) {
+        return Text('PAGE BUILDER');
+      },
+    );
+  }
+
+  AlertDialog buildInfoPopup() {
+    return AlertDialog(
+      shape: OutlineInputBorder(borderRadius: BorderRadius.circular(16.0)),
+      title: Text('Info/General Help'),
+      content: SizedBox(
+        height: 400.0,
+        child: CupertinoScrollbar(
+          child: ListView(
+            padding: const EdgeInsets.all(10.0),
+            children: buildInfoTextBox(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> buildInfoTextBox() {
+    List<Widget> textBoxWidgets = [];
+
+    double verticalMargin = 10.0;
+
+    Container para1 = Container(
+      margin: EdgeInsets.symmetric(vertical: verticalMargin),
+      child: Text(
+        "Lalalalala info info info.\n" +
+            "Here's an example on how to use the app!\n" +
+            "This app is really cool.\n" +
+            "roblox (robocop)",
+      ),
+    );
+
+    Container gif1 = Container(
+      margin: EdgeInsets.symmetric(vertical: verticalMargin),
+      child: Image.asset(
+        'assets/gifs/Screen_Recording_20250619_212522_Roblox.gif',
+      ),
+    );
+
+    Container para2 = Container(
+      margin: EdgeInsets.symmetric(vertical: verticalMargin),
+      child: Text(
+        'Roblox lmao' +
+            'wwdwadwdawaddwaadswwdwadwdawaddwaadswwdwadwdawaddwaadswwdwadwdawaddwaads' +
+            'wasdwarwwwwdwadwdawaddwaadswwdwadwdawaddwaadsdwadwdawaddwaadswwdwadw' +
+            'dawaddwaadswwdwadwdawaddwaadswwdwadwdawaddwaadsdawsdw' +
+            'dawaddwaadswwdwadwdawaddwaadswwdwadwdawaddwaadsdawsdw' +
+            'dawaddwaadswwdwadwdawaddwaadswwdwadwdawaddwaadsdawsdw' +
+            'dawaddwaadswwdwadwdawaddwaadswwdwadwdawaddwaadsdawsdw' +
+            'dawaddwaadswwdwadwdawaddwaadswwdwadwdawaddwaadsdawsdw' +
+            'dawaddwaadswwdwadwdawaddwaadswwdwadwdawaddwaadsdawsdw' +
+            'dawaddwaadswwdwadwdawaddwaadswwdwadwdawaddwaadsdawsdw',
+      ),
+    );
+
+    textBoxWidgets.add(para1);
+    textBoxWidgets.add(gif1);
+    textBoxWidgets.add(para2);
+
+    return textBoxWidgets;
+  }
 
   void geoFenceButtonPressed() {}
 
