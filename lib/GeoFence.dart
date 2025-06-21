47b96e02d22d8a637ebeb79208974f3d1844dac8
Racheet Bedi <racheetbedi25@gmail.com>
2025-06-21 18:25:45 -0500

    Integrated and imported the Google Maps API Dart Package

--- lib/GeoFence.dart
+++ lib/GeoFence.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class GeoFence extends StatelessWidget{
     const GeoFence({super.key});
