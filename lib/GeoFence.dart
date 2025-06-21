da57e0959ce711c4787a5b3028a02ff0262abf07
Racheet Bedi <racheetbedi25@gmail.com>
2025-06-21 01:42:00 -0500

    Set up the GeoFence page App Bar

--- lib/GeoFence.dart
+++ lib/GeoFence.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 
 class GeoFence extends StatelessWidget{
+    const GeoFence({super.key});
 
+    @override
     Widget build(BuildContext context){
-        
+        return Scaffold(
+            appBar: AppBar(
+                title: Text(
+                    'GeoFence',
+                    style: TextStyle(
+                    fontFamily: 'PlayfairDisplay',
+                    fontSize: 24.0,
+                    color: Theme.of(context).secondaryHeaderColor,
+                    ),
+                ),
+                
+                centerTitle: true,
+
+                backgroundColor: Theme.of(context).colorScheme.primary,
+            )
+        );
     }
     
 }
