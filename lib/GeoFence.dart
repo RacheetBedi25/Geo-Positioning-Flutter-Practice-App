217625ecaad51128c72e26f56266968042ad097f
Racheet Bedi <racheetbedi25@gmail.com>
2025-06-21 21:07:12 -0500

    Added map view to GeoFence page and added Add and Remove buttons on that page.

--- lib/GeoFence.dart
+++ lib/GeoFence.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class GeoFence extends StatelessWidget{
+class GeoFence extends StatefulWidget{
     const GeoFence({super.key});
+
+    @override
+    State createState(){
+        return _GeoFenceState();
+    }
+}
+
+class _GeoFenceState extends State<GeoFence>{
+
+    final double topGap = 170.0;
+    final double buttonGap = 40.0;
+
+    late GoogleMapController mapController;
+
+    LatLng _center = LatLng(-23.5557714, -46.6395571);
+
+    void _onMapCreated(GoogleMapController controller){
+        mapController = controller;
+    }
+
 
     @override
     Widget build(BuildContext context){
@@ -13,7 +34,7 @@ class GeoFence extends StatelessWidget{
                     'GeoFence',
                     style: TextStyle(
                     fontFamily: 'PlayfairDisplay',
-                    fontSize: 24.0,
+                    fontSize: 39.0,
                     color: Theme.of(context).secondaryHeaderColor,
                     ),
                 ),
@@ -21,8 +42,96 @@ class GeoFence extends StatelessWidget{
                 centerTitle: true,
 
                 backgroundColor: Theme.of(context).colorScheme.primary,
-            )
+            ),
+            body: Stack(
+
+                children: <Widget>[
+
+                    Positioned(
+
+                        top: 100,
+                        left:40,
+
+                        child: Container(
+
+                            constraints: BoxConstraints(
+                                maxHeight: 300,
+                                maxWidth: 300
+                            ),
+                            
+                            child: GoogleMap(
+                                    onMapCreated: _onMapCreated,
+                                    initialCameraPosition: CameraPosition(
+                                        target: _center,
+                                        zoom: 11.0
+                                    )
+                            ),
+                        )
+                    ),
+
+                    Positioned(
+
+                        top: 400,
+                        left: 50,
+
+                        child: Container(
+                            
+                            child: Column(
+                                children: [
+                                    createButtonRow(
+                                        context,
+                                        MainAxisAlignment.center,
+                                        EdgeInsets.only(top: 30),
+                                        (){
+
+                                        },
+                                        'Add GeoFence',
+                                    ),
+                                    createButtonRow(
+                                        context,
+                                        MainAxisAlignment.center,
+                                        EdgeInsets.only(top: 20),
+                                        (){
+
+                                        },
+                                        'Remove GeoFence'
+                                    ),
+                                ]
+                            )
+                        )
+                    ),
+                ]
+            ),
         );
     }
+
+    Row createButtonRow(
+    BuildContext context,
+    MainAxisAlignment mainAlignment,
+    EdgeInsets margin,
+    VoidCallback onPressed,
+    String text,)
+    {
+    return Row(
+      mainAxisAlignment: mainAlignment,
+      children: [
+        Container(
+          margin: margin,
+          width: 270,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Theme.of(context).primaryColor,
+              foregroundColor: Theme.of(context).primaryColorLight,
+            ),
+            onPressed: onPressed,
+            child: Text(
+              text,
+              style: TextStyle(fontFamily: 'PlayfairDisplay', fontSize: 25),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
     
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'Home.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 // https://pub.dev/packages/geolocator
 
